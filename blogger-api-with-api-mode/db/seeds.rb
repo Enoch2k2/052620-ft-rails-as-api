4ed602ecb3a0549cd2263a41267b506430cd603a
Enoch2k2 <enoch2k2@hotmail.com>
2020-09-02 10:51:25 -0500

    added rails with api mode

--- blogger-api-with-api-mode/db/seeds.rb
+++ blogger-api-with-api-mode/db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-(1..100).to_a.each do |num|
-  Blog.create(title: "Title #{num}", content: "Random content #{num}")
-end
--- blogger-api-without-api-mode/app/controllers/blogs_controller.rb
+++ blogger-api-without-api-mode/app/controllers/blogs_controller.rb
@@ -1,2 +1,50 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show]
+  skip_before_action :verify_authenticity_token, only: [:create, :update]
+
+  def index
+    @blogs = Blog.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @blogs }
+    end
+  end
+  
+  def show
+    respond_to do |format|
+      format.html
+      format.json { render json: @blog, except: [:id] }
+    end
+  end
+
+  def new
+    @blog = Blog.new
+  end
+  
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      respond_to do |format|
+        format.html { redirect_to blogs_path }
+        format.json { render json: @blog }
+      end  
+    else
+      respond_to do |format|
+        format.html { render :new }
+        format.json do
+           render json: { errors: @blog.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+  private
+    def set_blog
+      @blog = Blog.find_by_id(params[:id])
+    end
+
+    def blog_params
+      params.require(:blog).permit(:title, :content)
+    end
 end
